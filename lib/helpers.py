1b91819076af6670d268f9a8fe14967562b45369
bururun <bururun@users.noreply.github.com>
2025-11-23 17:07:12 +0200

    style: fix broken tests

---
+++ lib/helpers.py
@@ -0,0 +1,9 @@
+# Helper functions
+
+def helper_function_13(x):
+    """Helper function for iteration 13."""
+    return x * 13
+
+def format_output(data):
+    """Format output data."""
+    return str(data).upper()
